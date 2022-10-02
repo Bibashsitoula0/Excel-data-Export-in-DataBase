da6807382c2f3d778560d90d27fdb8f94a708ec1
Bibash sitoula <sitoulabibash666@gmail.com>
2022-10-02 17:53:09 +0545

    Update IStudentRepository.cs

--- IStudentRepository.cs
+++ IStudentRepository.cs
@@ -10,14 +10,6 @@ namespace UGC.DAL.StudentRepository
 {
     public interface IStudentRepository
     {
-        Task<List<StudentNeb>> GetStudent(int? pageNumber, int? pageSize);
-        Task<List<StudentNeb>> GetStudentById(int Id);
-        Task<List<StudentNeb>> GetStudentByNEBId(string nebID);
-        Task<int> InsertStudentAsync(StudentNeb model);
-        Task<bool> UpdateStudentAsync(StudentNeb model);
-        Task<bool> DeleteStudent(StudentNeb model);
-        Task<List<StudentNeb>> GetStudentNebAsyncId(long Id);
-        Task<List<StudentNeb>> GetStudentListInExcel();
         Task<bool> UploadExcel(string file);
     }
 }
